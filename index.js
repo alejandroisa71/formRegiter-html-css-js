b40d64ce9e1d260ca6f667502acfd847305661f6
unknown <alejandroisa71@gamail.com>
2023-06-05 12:14:28 -0300

    Initial validation form

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello')
+const form = document.querySelector('form')
+
+
+const validateInput = (x) =>{
+    
+    console.log(x)
+}
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    
+    for(let [name, value] of formData) {
+        alert(`${name} = ${value}`); // key1 = value1, luego key2 = value2
+      }
+    
+   
+    
+})
