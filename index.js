fa4c3e862479021bbf24f6b708252f61ecfd2459
unknown <alejandroisa71@gamail.com>
2023-06-06 12:29:45 -0300

    Probando FormData para validar campos

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const form = document.querySelector("form");
+let error=false
 
-
-
-const validateInput = (name,value) => {
-    if (!value || value.trim().length ===0) alert(`El campo ${name} no puede estar vacio`)
-    console.log(name,value)
-}
+// const validateInput = (name, value, error) => {
+//   if (!value){
+//     error =true
+//     return error
+//   }
+//     //alert(`El campo ${name} no puede estar vacio`);
+//   //console.log(name, value);
+// };
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
+  !formData.get('firstName') ? console.log('no hay') : console.log('si hay')
 
-  formData.forEach((value, name) => {
-    validateInput(name,value)
-  });
+  // formData.forEach((value, name) => {
+  //   validateInput(name, value.trim(), error);
+  // });
+  
 });
